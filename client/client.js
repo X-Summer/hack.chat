1fa8a8f17e277f0dd27e86c4f0f11d05bd110f77
X-Summer <x1515241@gmail.com>
2019-11-13 15:35:52 +0800

    Update client.js

--- client/client.js
+++ client/client.js
@@ -64,7 +64,7 @@ function join(channel) {
 	}
 	else {
 		// for local installs
-		ws = new WebSocket('ws://' + document.domain + ':6060/chat-ws')
+		ws = new WebSocket('wss://' + document.domain + ':6060/chat-ws')
 	}
 
 	var wasConnected = false
